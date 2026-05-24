7045a6fd48ee2cfbc726092bac49f3b6b8f78553
amalt7451 <amalt7451@gmail.com>
2026-05-24 23:57:29 +0530

    complete practice question b5

---
+++ b5-countAdult.js
@@ -0,0 +1,13 @@
+let students=[
+    {name:"amal",age:21},
+    {name:"john",age:15},
+    {name:"arya",age:19}
+];
+console.log(countAdult(students));
+function countAdult(){
+    let count = 0;
+    for(let i=0;i<students.length;i++){
+        if(students[i].age>= 18)count++;
+    }
+    return count;
+}
